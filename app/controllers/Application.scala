3e2b7e1227f3a4f0bb5141ae7e590fc6f19e3d06
Héctor Vizcaíno <hektor7@gmail.com>
2015-09-06 13:22:47 +0200

    Finding out how Scala binds variables from controller to views

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,11 @@
 package controllers
 
 import play.api.mvc._
+import play.api.templates.Html
 
 object Application extends Controller {
-
   def index = Action {
-    Ok(views.html.main())
+    val content = Html("<div>This is the content for the sample app<div>")
+        Ok(views.html.main("Home")(content))
   }
-
-}
+  }
